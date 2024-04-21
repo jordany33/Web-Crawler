b0cf8aac0ac3e87fa5a9b3de7f5a171f668ee9e3
otran2 <otran2@uci.edu>
2024-04-20 20:05:38 -0700

    Added in outline for pickle functions and moved over the directory level function Travis made

--- scraper.py
+++ scraper.py
@@ -1,11 +1,31 @@
 import re
+import pickle
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 
 seenURLs = set()
 words = {}
 alphaNum = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
-maxSize = -1
+maxSize = [-1, '']
+
+#Attempts to load all our global values from their stored pickle files if they exist, otherwise gives them default values
+def pickleLoad() ->None:
+    return
+
+#Attempts to save all our global values into their pickle files
+def pickleLoad() ->None:
+    return
+
+#Given a url, returns how many directories deep it is in it's path 
+def directory_length(url) -> int:
+    parsed_url=urlparse(url)
+
+    if isinstance(parsed_url.path, bytes):
+        path_str = parsed_url.path.decode('utf-8')
+    else:
+        path_str = parsed_url.path
+
+    return len(path_str.split('/')) - 1
 
 #Reads the content and returns a list of the alphanumeric tokens within it
 def tokenize(content: str) -> list:
@@ -35,23 +55,38 @@ def tokenize(content: str) -> list:
         tokens.append(curTok)
     return tokens
 
-#Our compute word frequencies from Assignment 1, we will use it to update our global dictionary of tokens using the token list
-def compute_word_frequencies(token_list: list) -> None:
+def compute_word_frequencies(token_list: list) -> dict:
     """
+    Runtime Complexity: O(M), M is the number of tokens in token_list.
+        This runtime is average-case.
+        The function iterates through each token in token_list once.
+        Dictionary operations are O(1).
+        There is constant time per token.
+
     Counts and returns the number of occurrences of each token in the 
     token list using a dictionary.
 
     :param tokens: List of tokens 
     :return: dict, mapping each token to the number of occurrences
     """
-    #Checks to see if token is in words, updates the count if it is, otherwise sets the count to 1
-    for i in token_list:
-        if i in words:
-            words[i] += 1
-        else:
-            words[i] = 1
-    return
+    frequencies = {}
 
+    for i in token_list:
+        if i in frequencies:
+            frequencies[i] += 1
+        else:
+            frequencies[i] = 1
+
+    return frequencies
+
+#Updates our global dictionary words frequency count for tokens using dic2
+def updateDict(dic2:dict) -> None:
+    global words
+    for x in dic2:
+        if x in words:
+            words[x] += dic2[x]
+        else:
+            words[x] = dic2[x]
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -80,11 +115,17 @@ def extract_next_links(url, resp):
     if html_content:
         global maxSize
         html_parsed = BeautifulSoup(html_content, 'lxml')
-        #Tokenize the string, then update the max_size variable and the frequency count of tokens
+        #Tokenize the string, then update the max_size variables
         tokens = tokenize(html_parsed.get_text())
-        if maxSize == -1 or maxSize < len(tokens):
-            maxSize = len(tokens)
-        compute_word_frequencies(tokens)
+        #Only crawl if there is a reasonable level of content, otherwise don't crawl, done by checking first the number of tokens
+        #to make sure they reach a certain threshold
+        if len(tokens) < 250:
+            return []
+        if maxSize[0] == -1 or maxSize[0] < len(tokens):
+            maxSize[0] = len(tokens)
+            maxSize[1] = url
+        newFreqs = compute_word_frequencies(tokens)
+        updateDict(newFreqs)
         #Create a stats.txt if it doesn't exist, otherwise overwrite it
         stats = open("stats.txt", "w")
         print(f"Current token list: {words}", file = stats)
@@ -113,6 +154,9 @@ def is_valid(url):
     # There are already some conditions that return False.
     try:
         if url in seenURLs:
+            return False
+        #If you go really deep into directories, at a certain threshold you are probably going into a trap
+        if directory_length(url) > 10:
             return False
         seenURLs.add(url)
         parsed = urlparse(url)
